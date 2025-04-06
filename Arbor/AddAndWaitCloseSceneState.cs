37ab4c1fa646138b8e524657a6569eed3b389fa0
matsufriends <pikapikan0526@gmail.com>
2025-04-06 16:55:40 +0900

    AddAndWaitCloseSceneの追加

---
+++ Arbor/AddAndWaitCloseSceneState.cs
@@ -0,0 +1,27 @@
+﻿#if USE_ARBOR
+using Arbor;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MornScene
+{
+    public class AddAndWaitCloseSceneState : StateBehaviour
+    {
+        [SerializeField] private MornSceneObject _scene;
+        [SerializeField] private StateLink _next;
+
+        public override async void OnStateBegin()
+        {
+            await SceneManager.LoadSceneAsync(_scene, LoadSceneMode.Additive);
+            var scene = SceneManager.GetSceneByName(_scene);
+            while (scene.isLoaded)
+            {
+                await UniTask.Yield(CancellationTokenOnEnd);
+            }
+
+            Transition(_next);
+        }
+    }
+}
+#endif
